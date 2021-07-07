cebcd748ee415132aa405c9563a5e1a9bb68c2a8
Igor Ferreira <igorcferreira@hotmail.com>
2021-07-08 01:12:00 +0200

    Separate sample from library

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(name: "print-message",
                 dependencies: ["ArgumentParserUsage"],
-                path: "Sources/PrintMessage"),
+                path: "Sample"),
         .target(
             name: "ArgumentParserUsage",
             dependencies: [
