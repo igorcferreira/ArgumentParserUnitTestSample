ea5ae09c4bdef202bda7a9ffe4b60b8bdbf1ece6
Igor Ferreira <igorcferreira@hotmail.com>
2021-07-07 23:33:56 +0200

    Create command

--- Sources/ArgumentParserUsage/ArgumentParserUsage.swift
+++ Sources/ArgumentParserUsage/ArgumentParserUsage.swift
@@ -1,3 +1,44 @@
-struct ArgumentParserUsage {
-    var text = "Hello, World!"
+import Foundation
+import ArgumentParser
+
+enum PrintError: LocalizedError {
+    case emptyMessage
+    
+    var errorDescription: String? { "Please, inform a message" }
+}
+
+struct PrintMessage: ParsableCommand {
+    
+    /// The `CodingKeys` is necessary to remove the `console` variable from the Decodable
+    /// parsing.
+    enum CodingKeys: String, CodingKey {
+        case message
+        case skipPrefix
+        case prefix
+    }
+    
+    @Flag(help: "Used to skip the prefix")
+    var skipPrefix = false
+    
+    @Option(name: .shortAndLong, help: "Prefix to be used in the message")
+    var prefix: String?
+    
+    @Argument(help: "The text to print")
+    var message: String
+    
+    /// This `console` variable is used to allow unit testing the output of the process.
+    var console: ((String) -> Void) = {
+        print($0)
+    }
+    
+    func run() throws {
+        
+        guard !message.isEmpty else { throw PrintError.emptyMessage }
+        
+        if skipPrefix {
+            console(message)
+        } else {
+            console("\(prefix ?? "Message: ")\(message)")
+        }
+    }
 }
