1402b9e40f6ef11e0bf26c2092ec1f797b1fb6e9
Igor Ferreira <igorcferreira@hotmail.com>
2021-07-07 23:34:04 +0200

    Implement tests

--- Tests/ArgumentParserUsageTests/ArgumentParserUsageTests.swift
+++ Tests/ArgumentParserUsageTests/ArgumentParserUsageTests.swift
@@ -2,14 +2,55 @@ import XCTest
 @testable import ArgumentParserUsage
 
 final class ArgumentParserUsageTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(ArgumentParserUsage().text, "Hello, World!")
+    
+    func testNoPrefix() throws {
+        var runner = try PrintMessage.parse(["Test Message", "--skip-prefix"])
+        runner.console = {
+            XCTAssertEqual($0, "Test Message", "Invalid message option")
+        }
+        try runner.run()
+    }
+    
+    func testDefaultPrefix() throws {
+        var runner = try PrintMessage.parse(["Test Message"])
+        runner.console = {
+            XCTAssertEqual($0, "Message: Test Message", "Invalid message option")
+        }
+        try runner.run()
+    }
+    
+    func testCustomPrefix() throws {
+        var runner = try PrintMessage.parse(["Test Message", "--prefix", "Another prefix: "])
+        runner.console = {
+            XCTAssertEqual($0, "Another prefix: Test Message", "Invalid message option")
+        }
+        try runner.run()
+    }
+    
+    func testSkipCustomPrefix() throws {
+        var runner = try PrintMessage.parse(["Test Message", "--prefix", "Another prefix: ", "--skip-prefix"])
+        runner.console = {
+            XCTAssertEqual($0, "Test Message", "Invalid message option")
+        }
+        try runner.run()
     }
 
+    func testNoArgumentException() throws {
+        XCTAssertThrowsError(try PrintMessage.parse([]))
+    }
+    
+    func testEmptyMessage() throws {
+        var runner = try PrintMessage.parse([""])
+        runner.console = { _ in
+            XCTFail("No message should be displayed")
+        }
+        XCTAssertThrowsError(try runner.run())
+    }
+    
     static var allTests = [
-        ("testExample", testExample),
+        ("testNoPrefix", testNoPrefix),
+        ("testDefaultPrefix", testDefaultPrefix),
+        ("testCustomPrefix", testCustomPrefix),
+        ("testNoArgumentException", testNoArgumentException)
     ]
 }
